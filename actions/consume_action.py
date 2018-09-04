b0e32eb0d0730dc9ef674f755420779ac12a1772
DrewHoo <drewhoover@gmail.com>
2018-09-03 22:02:01 -0500

    Enable creatures attacking creatures

--- actions/consume_action.py
+++ actions/consume_action.py
@@ -15,19 +15,34 @@ def consume_action(state, action):
 def attack(old_state, action):
     state = deepcopy(old_state)
     player = action['player']
+    opposing_player = 'p1' if player == 'p2' else 'p2'
     lane = action['lane']
     lane_index = action['lane_index']
     attacking_creature = state[player][lane][lane_index]
+
     if not attacking_creature['can_attack']:
         print('invalid move; {} cannot attack'.format(attacking_creature['name']))
     else:
         state[player][lane][lane_index]['can_attack'] = False
 
     attack_power = attacking_creature['power']
-    if player == 'p1':
-        state['p2']['health'] -= attack_power
+    if 'target' in action:
+        target = action['target']
+        target_creature = state[opposing_player][lane][target['lane_index']]
+        
+        if target_creature['health'] <= attack_power:
+            state[opposing_player][lane].pop(target['lane_index'])
+            state[opposing_player]['discard_pile'].insert(0, target_creature)
+        else:
+            target_creature['health'] -= attack_power
+        
+        if attacking_creature['health'] <= target_creature['power']:
+            state[player][lane].pop(lane_index)
+            state[player]['discard_pile'].insert(0, attacking_creature)
+        else:
+            attacking_creature['health'] -= target_creature['power']
     else:
-        state['p1']['health'] -= attack_power
+        state[opposing_player]['health'] -= attack_power
     
     return state
 
--- test/test_actions.py
+++ test/test_actions.py
@@ -35,12 +35,11 @@ def test_consume_action_play_creature():
     assert new_state['p1']['shadow_lane'] == [old_state['p1']['hand'][0]]
 
 
-def test_consume_action_attack():
+def test_consume_action_attack_face():
     old_state = deepcopy(DEFAULT_STATE)
     action = {
         'type': 'attack',
         'player': 'p1',
-        'target': 'face',
         'lane': 'field_lane',
         'lane_index': 0
     }
@@ -57,3 +56,38 @@ def test_consume_action_attack():
     assert new_state != old_state
     assert new_state['p1']['field_lane'][0]['can_attack'] == False
     assert new_state['p2']['health'] == 25
+
+
+def test_consume_action_attack_creature():
+    mudcrab_merchant = {
+        'power': 5,
+        'health': 1,
+        'can_attack': True,
+        'name': 'Mudcrab Merchant'
+    }
+    creeper = {
+        'power': 1,
+        'health': 1,
+        'can_attack': False,
+        'name': 'Creeper'
+    }
+    old_state = deepcopy(DEFAULT_STATE)
+    action = {
+        'type': 'attack',
+        'player': 'p1',
+        'target': {'lane_index': 0},
+        'lane': 'field_lane',
+        'lane_index': 0
+    }
+    p1 = deepcopy(DEFAULT_PLAYER_STATE)
+    p1.update({'field_lane': [mudcrab_merchant]})
+    p2 = deepcopy(DEFAULT_PLAYER_STATE)
+    p2.update({'field_lane': [creeper]})
+    old_state.update({'p1': p1, 'p2': p2})
+    new_state = consume_action(old_state, action)
+
+    assert new_state != old_state
+    assert new_state['p1']['field_lane'] == []
+    assert new_state['p2']['field_lane'] == []
+    assert new_state['p1']['discard_pile'][0]['name'] == 'Mudcrab Merchant'
+    assert new_state['p2']['discard_pile'][0]['name'] == 'Creeper'
